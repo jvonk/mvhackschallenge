aeb196ef1f9046c8281f4c6869a885bc83f20991
Johan Vonk <johan.d.s.vonk@gmail.com>
2019-05-05 13:24:30 -0700

    updates

--- qr-reader/App.js
+++ qr-reader/App.js
@@ -1,61 +1,82 @@
 import React from 'react';
-import { Alert, Button, Dimensions, LayoutAnimation, Linking, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, Dimensions, Linking, ListView, StatusBar, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import { BarCodeScanner, Permissions } from 'expo';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import Swipeout from 'react-native-swipeout';
 
+var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
 class HomeScreen extends React.Component {
   state = {
     url: 'NONE',
+    dataSource: ds,
+    hasCamera: null,
+    _data: new Array(),
   };
+  deleteData = (rowData) => {
+    let val = this.state._data.indexOf(rowData);
+    if (val<=0)return;
+    this.state._data.splice(val-1, 1);
+    this.setState({ dataSource: this.state.dataSource.cloneWithRows(this.state._data) });
+  }
+  
+  renderRow = (rowData, rowID) => {
+    let swipeBtns = [{
+      text: 'Delete',
+      backgroundColor: 'red',
+      underlayColor: 'rgba(0, 0, 0, 0.6)',
+      onPress: this.deleteData.bind(this, rowData)
+    }];
+    const uri = rowData;
+    return (
+      <Swipeout
+        right={swipeBtns}
+        underlayColor='rgba(192,192,192,0.6)'
+        autoClose={true}>
+        <TouchableHighlight
+          key={rowID}
+          onPress={() => Linking.openURL(rowData)}
+          underlayColor='rgba(192,192,192,0.6)'>
+          <View style={styles.rowContainer}>
+            <Text style={styles.note}> {rowData} </Text>
+          </View>
+        </TouchableHighlight>
+      </Swipeout>
+    )
+  }
+
+  componentDidMount() {
+    if (this.state.url.data && !this.state._data.includes(this.state.url.data)) {
+      this.state._data.push(this.state.url.data);
+      this.setState({ dataSource: this.state.dataSource.cloneWithRows(this.state._data) });
+    }
+  }
+
   render() {
-    const { navigation } = this.props;
     const { navigate } = this.props.navigation;
+    const { navigation } = this.props;
     this.state.url = navigation.getParam('url', this.state.url);
+    if (navigation.getParam('hasCamera', this.state.hasCamera)) {
+      this.state.hasCamera= true;
+    }
+    if (this.state.url.data && !this.state._data.includes(this.state.url.data)) {
+      this.state._data.push(this.state.url.data);
+      this.setState({ dataSource: this.state.dataSource.cloneWithRows(this.state._data) });
+    }
     return (
       <View style={styles.container}>
         <Button
           title="Go to Camera"
-          onPress={() => navigate('Camera')}
+          onPress={() => navigate('Camera', {hasCamera: this.state.hasCamera})}
         />
-        {this.state.url=='NONE'
-        ?<View></View>
-        :
-        <View style={styles.bottomBar}>
-          <ScrollView
-            horizontal={true}
-            keyboardShouldPersistTaps='always'>
-            <TouchableOpacity activeOpacity={1} style={styles.url} onPress={this._handlePressUrl}>
-              <Text numberOfLines={1} style={styles.urlText}>
-                {this.state.url.data}
-              </Text>
-            </TouchableOpacity>
-          </ScrollView>
-        </View>
-        }
+        <ListView
+          dataSource={this.state.dataSource}
+          enableEmptySections={true}
+          renderRow={this.renderRow}/>
         <StatusBar hidden />
       </View>
     );
   }
-
-  _handlePressUrl = () => {
-    Alert.alert(
-      'Open this URL?',
-      this.state.url.data,
-      [
-        {
-          text: 'Yes',
-          onPress: () => Linking.openURL(this.state.url.data),
-        },
-        { text: 'No', onPress: () => { } },
-      ],
-      { cancellable: false }
-    );
-  };
-
-  _handlePressCancel = () => {
-    this.setState({ lastURL: null });
-  };
 }
 
 class CameraScreen extends React.Component {
@@ -64,7 +85,9 @@ class CameraScreen extends React.Component {
   };
 
   componentDidMount() {
-    this._requestCamera();
+    const { navigation } = this.props;
+    if (navigation.getParam('hasCamera', this.state.hasCamera)) this.setState({ hasCamera: true });
+    else this._requestCamera();
   }
 
   _requestCamera = async () => {
@@ -76,7 +99,7 @@ class CameraScreen extends React.Component {
 
   _handleBarCodeRead = result => {
     const { navigate } = this.props.navigation;
-    navigate('Home', { url: result });
+    navigate('Home', { url: result, hasCamera: this.state.hasCamera });
   };
 
   render() {
@@ -104,25 +127,18 @@ class CameraScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
     backgroundColor: '#fff',
   },
-  bottomBar: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(0,0,0,0.5)',
+  rowContainer: {
+    padding: 10,
+  },
+  note: {
+    flex: 2,
+    fontSize: 22,
     padding: 15,
-    flexDirection: 'row',
-  },
-  url: {
-    flex: 1,
-  },
-  urlText: {
-    color: '#fff',
-    fontSize: 20,
+    color: "#000",
   },
 });
 
